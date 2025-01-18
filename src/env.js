e968b8a2f6d310eab4c1832777c46b0ab2764365
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2025-01-17 22:44:16 -0700

    use experimental --env-file param instead of dotenv

--- src/env.js
+++ src/env.js
@@ -1,17 +1,16 @@
-import { config } from "dotenv";
-const {parsed} = config()
+import process from 'node:process'
 /** @type {string} */
-export const R2_ACCESS_KEY = parsed["R2_ACCESS_KEY"] || "";
+export const R2_ACCESS_KEY = process.env["R2_ACCESS_KEY"] || "";
 if (!R2_ACCESS_KEY) throw "no access key";
 
 /** @type {string} */
-export const R2_SECRET_ACCESS_KEY = parsed["R2_SECRET_ACCESS_KEY"] || "";
+export const R2_SECRET_ACCESS_KEY = process.env["R2_SECRET_ACCESS_KEY"] || "";
 if (!R2_SECRET_ACCESS_KEY) throw "no secret access key";
 
 /** @type {string} */
-export const R2_ENDPOINT = parsed["R2_ENDPOINT"] || "";
+export const R2_ENDPOINT = process.env["R2_ENDPOINT"] || "";
 if (!R2_ENDPOINT) throw "no endpoint";
 
 /** @type {string} */
-export const R2_BUCKET = parsed["R2_BUCKET"] || "";
+export const R2_BUCKET = process.env["R2_BUCKET"] || "";
 if (!R2_BUCKET) throw "no bucket";
